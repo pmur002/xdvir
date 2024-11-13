30c06513774f1768b2ca74ec1f5b69d09f023eae
Paul Murrell <paul@stat.auckland.ac.nz>
2024-11-13 16:51:49 +1300

    bug in initTinyTeX();  unique name for no-TeX workflow

--- R/tinytex.R
+++ R/tinytex.R
@@ -7,7 +7,7 @@ initTinyTeX <- function() {
     } else {
         ## Assume that, if 'latex' is on the PATH, then {tinytex} can see it
         TeX <- Sys.which("latex")
-        if (nchar(TeX > 0)) {
+        if (nchar(TeX) > 0) {
             set("texVersion", TeX)
         }
     }
