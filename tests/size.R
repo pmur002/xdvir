8c5e24cf3d8542e86846045aa8750d6d28c65b59
Paul Murrell <paul@stat.auckland.ac.nz>
2024-11-28 13:37:48 +1300

    investigate runner failures

--- tests/size.R
+++ tests/size.R
@@ -5,6 +5,14 @@ library(grid)
 ## Make debugging information available
 options(tinytex.verbose=TRUE, xdvir.quiet=FALSE)
 
+if (nchar(Sys.getenv("GITHUB_RUN_ID"))) {
+    ## For testing on github runners, create files within .Rcheck
+    ## directory so that we can easily return them as artifacts
+    texFile <- "test-size.tex"
+} else {
+    texFile <- NULL
+}
+
 if (Sys.info()["sysname"] == "Darwin") {
     png("size-%02d.png", type="quartz")
 } else {
@@ -14,7 +22,7 @@ if (Sys.info()["sysname"] == "Darwin") {
 
 if (xdvir:::haveTeX()) {
     grid.newpage()
-    x <- latexGrob("\\Huge test")
+    x <- latexGrob("\\Huge test", texFile=texFile)
     grid.draw(x)
     grid.rect(width=grobWidth(x), height=grobHeight(x), gp=gpar(fill=NA))
     grid.circle(grobX(x, "west"), grobY(x, "west"), r=unit(1, "mm"),
