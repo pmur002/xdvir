c9fba539898bbae54332e26084817852113bce76
Paul Murrell <paul@stat.auckland.ac.nz>
2026-01-15 15:47:58 +1300

    add convenience for specifying variable fonts via fontspecPackage()

--- R/fontspec.R
+++ R/fontspec.R
@@ -1,6 +1,26 @@
 
-fontspecPreamble <- function(font=NULL) {
+fontspecPreamble <- function(font=NULL, renderer=NULL, axes=NULL) {
     preamble <- "\\usepackage{fontspec}"
+    defaults <- NULL
+    if (!is.null(renderer) && tolower(renderer) == "harfbuzz")
+        defaults <- c(defaults, "Renderer=Harfbuzz")
+    if (!is.null(axes)) {
+        variations <- fontVariation(axes)
+        if (is.null(variations)) {
+            warning("Ignoring unsupported font axes")
+        } else {
+            defaults <- c(defaults,
+                          paste0("RawFeature={axis={",
+                                 paste(names(variations), variations, sep="="),
+                                 "}}"))
+        }
+    }
+    if (!is.null(defaults)) {
+        preamble <- c(preamble,
+                      paste0("\\defaultfontfeatures{",
+                             paste(defaults, collapse=","),
+                             "}"))
+    }
     if (!is.null(font)) {
         if (dirname(font) == ".") {
             setfont <- paste0("\\setmainfont{", font, "}")
@@ -14,11 +34,15 @@ fontspecPreamble <- function(font=NULL) {
     preamble
 }
 
-fontspecPackage <- function(font=NULL, name=font) {
+fontspecPackage <- function(font=NULL, name=font, renderer=NULL, axes=NULL) {
     if (is.null(font)) {
-        LaTeXpackage(name="fontspec", preamble=fontspecPreamble())
+        LaTeXpackage(name="fontspec",
+                     preamble=fontspecPreamble(renderer=renderer))
     } else {
-        LaTeXpackage(name=name, preamble=fontspecPreamble(font))
+        LaTeXpackage(name=name,
+                     preamble=fontspecPreamble(font=font,
+                                               renderer=renderer,
+                                               axes=axes))
     }
 }
 
